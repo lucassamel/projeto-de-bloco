b03f7bf47dd77c1068766ecc7ab4db41c92904e4
lucassamel <lucas.samel97@gmail.com>
2020-03-26 20:27:33 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+
 
 export default {
   name: 'App',
+  data: function(){
+    return{};
+  },
   components: {
-    HelloWorld
+    Header,
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.selector-for-some-widget {
+  box-sizing: content-box;
 }
 </style>
